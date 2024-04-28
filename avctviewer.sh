b97f935b103dce8e06ebe83c53f6e77c60f4454b
Roger Gammans <rgammans@gammascience.co.uk>
2024-04-28 15:41:55 +0100

    Simplified shell script for experimenting with the Lenovo Units
    See #2

--- avctviewer.sh
+++ avctviewer.sh
@@ -4,8 +4,7 @@ JAVAPATH=$APPDIR/usr/jre1.6.0_27/
 man() {
  cat - >&2 <<EOF
 NAME
-   $ARGV0 - Connect to an Avocent (or OEM) IP KVM Switch
-
+   $ARGV0 - Connect to an Lenovo IP KVM Switch
 
 
 EOF
@@ -15,71 +14,23 @@ EOF
 usage() {
     
  cat - >&2 <<EOF
-$ARGV0 SERVERNAME KVMHOST -p portnumber
-$ARGV0 SERVERNAME KVMHOST -r DSQRId
+$ARGV0 arguments [option=value]*
 
 Options:
-    -p  Portnumber that the required server is connected to.
-    -r  Unique Id of servers KVM 'dongle'
-    -t  Alternate title for window
-    -u  Username ( default: Admin)
-    -P  Password (default is blank)
+    ip: Ip address of KVM switch
+    helpurl: ?
+    user: Numeric user id in hex. Eg 0x66334873
+    passwd: password
+    apcp: Must be '1'
+    version: Must be '2'
+    kmport: ?
+    vport: ?
+    title: Title for window.
 EOF
 }
 
-fatal() {
-    for i; do
-        echo -e "${i}" >&2
-    done
-    exit 1
-}
-
-UNITSPEC=""
-TITLE=""
-USERNAME="Admin"
-OEM="HP"
-
-optspec="hp:r:t:u:P:"
-
-
-SERVERNAME=$1; 
-case  "x$SERVERNAME" in
-    x-h)  ;;
-    x-*|x) fatal "Expected Servername before options" ;;
-    *) shift ;;
-esac
-
-IPADDRESS=$1; 
-case  "x$IPADDRESS" in
-    x-h)  ;;
-    x-*|x) fatal "Expected Hostname before options" ;;
-    *) shift ;;
-esac
-
-while getopts "$optspec" optchar ; do
-    case "$optchar" in
-        h) usage; exit 0 ;;
-        p) UNITSPEC="p:$OPTARG" ;;
-        r) UNITSPEC="r:$OPTARG" ;;
-        t) TITLE=$OPTARG ;;
-        o) OEM=$OPTARG ;;
-        u) USERNAME=$OPTARG ;;
-        P) PASSWD=$OPTARG; shift ;;
-        *) fatal  "Unknown option: '-${optchar}'" "See '${0} -h for usage" ;;
-    esac
-done
-
-TITLE=${TITLE:=$SERVERNAME}
-
-# Debugging!
-#echo "Viewer with $SERVERNAME  , $IPADDRESS, $OEM, $UNITSPEC, $TITLE, $USERNAME, $PASSWD"
-#echo LD_LIBRAY_PATH=$JAVAPATH/lib/amd64 $JAVAPATH/bin/java -Djava.security.properties=$APPDIR/etc/java-6-openjdk/security/java.security -Duser.variant=$OEM -Doem.variant=$OEM -cp './share/avctviewer/*' \
-#      com.avocent.video.Nautilus\
-#      devicetype=avsp  oem=$OEM path="a:$IPADDRESS,$UNITSPEC,s:$SERVERNAME,e:1,c:0" user=$USERNAME password=$PASSWD title=$SERVERNAME
-#
-## The 'c:' element of Path is channel; but channel 0 seems to be what need to alwts be specified
-LD_LIBRAY_PATH=$JAVAPATH/lib/amd64 $JAVAPATH/bin/java -Djava.security.properties=$APPDIR/etc/java-6-openjdk/security/java.security -Duser.variant=$OEM -Doem.variant=$OEM -cp './share/avctviewer/*' \
-      com.avocent.video.Nautilus\
-      devicetype=avsp  oem=$OEM path="a:$IPADDRESS,$UNITSPEC,s:$SERVERNAME,e:1,c:0" user=$USERNAME password=$PASSWD title=$SERVERNAME
+LD_LIBRAY_PATH=$JAVAPATH/lib/amd64 $JAVAPATH/bin/java -Djava.security.properties=$APPDIR/etc/java-6-openjdk/security/java.security  -cp './share/avctviewer/*' \
+      com.avocent.ibmc.kvm.Main\
+      "$@"
 
 
